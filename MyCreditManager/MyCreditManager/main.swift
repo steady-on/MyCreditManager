32da7dde31e4e3d51d5404f294114ab95dcb221b
steady-on <roen.steady.on@gmail.com>
2023-04-30 15:37:25 +0900

    feat: startProgram 함수 작성

--- MyCreditManager/MyCreditManager/main.swift
+++ MyCreditManager/MyCreditManager/main.swift
@@ -7,5 +7,41 @@
 
 import Foundation
 
-print("Hello, World!")
+var students: [String: [String: String]] = [:]
 
+func startProgram() {
+    while true {
+        print("원하는 기능의 숫자를 입력해주세요.")
+        print("1: 학생추가, 2: 학생삭제, 3: 성적추가(수정), 4: 성적삭제, 5: 성적확인, X: 종료")
+        
+        let menuChoice = readLine()
+        
+        switch menuChoice {
+        case "1":
+            print("학생추가")
+            continue
+        case "2":
+            print("학생삭제")
+            continue
+        case "3":
+            print("성적추가(수정)")
+            continue
+        case "4":
+            print("성적삭제")
+            continue
+        case "5":
+            print("성적확인")
+            continue
+        case "X", "x":
+            print("프로그램을 종료합니다...")
+            break
+        default:
+            print("입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요.")
+            continue
+        }
+        
+        break
+    }
+}
+
+startProgram()
