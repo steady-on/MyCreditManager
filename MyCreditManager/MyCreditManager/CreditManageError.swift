e031ffe449825cddbd7b5b0e8ce46dccb8603a40
steady-on <roen.steady.on@gmail.com>
2023-04-30 23:48:36 +0900

    refactor(CreditManageError): 매개변수 추가
    - 에러메세지에 추가하여 출력될 변수 데이터(ex. 이름)를 에러객체게 매개인자로 넘겨서 출력되도록 수정

--- MyCreditManager/MyCreditManager/CreditManageError.swift
+++ MyCreditManager/MyCreditManager/CreditManageError.swift
@@ -22,11 +22,11 @@ enum FunctionType {
 enum CreditManageError: Error {
     case invalidInput(InputType)
     case emptyStudents(FunctionType)
-    case existStudent
-    case notFoundStudent
+    case existStudent(name: String)
+    case notFoundStudent(name: String)
     case invalidGradeInput
     case notFoundSubject(name: String, subject: String)
-    case emptyCredits
+    case emptyCredits(name: String)
     
     var localizedDescription: String {
         switch self {
@@ -42,16 +42,16 @@ enum CreditManageError: Error {
             return "성적을 삭제할 학생이 없습니다. 먼저 학생을 추가해 주세요."
         case .emptyStudents(.check):
             return "성적을 조회할 학생이 없습니다. 먼저 학생을 추가해 주세요."
-        case .existStudent:
-            return "학생의 데이터는 이미 존재하므로 추가되지 않습니다."
-        case .notFoundStudent:
-            return "학생을 찾을 수 없습니다."
+        case .existStudent(let name):
+            return "\(name) 학생의 데이터는 이미 존재하므로 추가되지 않습니다."
+        case .notFoundStudent(let name):
+            return "\(name) 학생을 찾을 수 없습니다."
         case .invalidGradeInput:
             return "올바르지 않은 성적이 입력되었습니다. 다시 확인해 주세요."
         case .notFoundSubject(let name, let subject):
             return "\(name) 학생의 \(subject)에 대한 성적을 찾을 수 없습니다. 다시 확인해 주세요."
-        case .emptyCredits:
-            return "학생은 아직 입력된 성적이 없습니다. 성적을 추가해 주세요."
+        case .emptyCredits(let name):
+            return "\(name) 학생은 아직 입력된 성적이 없습니다. 성적을 추가해 주세요."
         }
     }
 }
