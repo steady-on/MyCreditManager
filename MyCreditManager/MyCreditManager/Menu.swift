6f6d012419c182c3b066a02d0cee334cf3a4601f
steady-on <roen.steady.on@gmail.com>
2023-05-10 01:12:16 +0900

    feat: Menu 열거형 구현
    - 프로그램의 메뉴인 addStudent, deleteStudent, updateStudent, deleteCredit, checkScore, exitProgram를 case 가지는 열거형
    - rawValue로 입력값인 1~5, 프로그램의 종료를 의미하는 X를 가지도록 구현

---
+++ MyCreditManager/MyCreditManager/Menu.swift
@@ -0,0 +1,17 @@
+//
+//  Menu.swift
+//  MyCreditManager
+//
+//  Created by Roen White on 2023/05/10.
+//
+
+import Foundation
+
+enum Menu: String {
+    case addStudent = "1"
+    case deleteStudent = "2"
+    case updateCredit = "3"
+    case deleteCredit = "4"
+    case checkScore = "5"
+    case exitProgram = "X"
+}
